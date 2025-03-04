30cfefdb01ac99e1fd60aac0cc2d06d545907726
Chris Howard <chrish@341design.com.au>
2025-03-04 13:56:41 +1100

    fix router dom error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Map } from './components/Map';
 import { usePreline } from './utils/preline';
 
